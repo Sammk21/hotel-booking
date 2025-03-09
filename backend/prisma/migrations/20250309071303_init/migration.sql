21d06eb6d9f15a5c3cacea4130be0f730715e5ec
sameer khan <samkhan@sameers-MacBook-Air.local>
2025-03-09 12:49:49 +0530

    added images link nad updated readme.md

---
+++ backend/prisma/migrations/20250309071303_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `updatedAt` on the `aadhaar` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "aadhaar" DROP COLUMN "updatedAt";
